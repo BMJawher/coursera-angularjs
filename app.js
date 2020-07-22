b2e79d619c1d50f777da146e40e275fc54721886
ameniJAT <manou2020.jt@gmail.com>
2020-07-22 09:57:59 +0100

    new folder

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+  angular.module('myFirstApp', [])
+
+  .controller('MyFirstController', function($scope) {
+
+    // $scope.name = "Jaouher";
+    // $scope.sayHello = function() {
+    //   return "Hello Coursera!";
+    // };
+console.log($scope)
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function () {
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    }
+
+    function calculateNumericForString(string){
+      var totalStringValue = 0;
+      for (var i=0; i<string.length; i++){
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    }
+
+  })
+
+})();
